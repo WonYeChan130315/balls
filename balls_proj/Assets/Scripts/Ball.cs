a8f056234655ee523aaaa4917e14ebc5d1e117e3
wonyechan <wonyechan7772@gmail.com>
2023-07-06 21:44:45 +0900

    close #2, 3

--- balls_proj/Assets/Scripts/Ball.cs
+++ balls_proj/Assets/Scripts/Ball.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    public GameObject linePrefab;
+    public GameObject[] ballPrefabs;
     public float speed;
 
     private Vector2 lastVelocity;
     private Rigidbody2D rigidbody;
     private bool click;
+    private bool noClick;
     private bool collision;
     private bool enterClick;
 
     private void Awake() {
         rigidbody = GetComponent<Rigidbody2D>();
-
-        Vector2 ramdomVec = Random.insideUnitSphere;
-        rigidbody.AddForce(ramdomVec.normalized * speed);
+        RandomBounce(transform);
     }
 
     private void Update() {
@@ -48,12 +47,23 @@ public class Ball : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D other) {
         if(other.gameObject.CompareTag("CheckBox")) {
             if(tag == "Ball") {
-                if(click) {
+                if(click && !noClick) {
+                    if(Random.Range(0, 6) == 0) AddBall();
+
+                    if(Random.Range(0, 11) == 0) {
+                        noClick = true;
+                        GetComponent<SpriteRenderer>().color = Color.white;
+                    } else {
+                        noClick = false;
+                        GetComponent<SpriteRenderer>().color = Color.blue;
+                    }
+                    
                     Bounce(other);
                     click = false;
+                } else if(!click && noClick) {
+                    Bounce(other);
                 } else {
-                    print("game over!");
-                    Time.timeScale = 0;
+                    Gameover();
                 }
             } else {
                 Bounce(other);
@@ -68,10 +78,25 @@ public class Ball : MonoBehaviour
         collision = false;
     }
 
+    private void Gameover() {
+        print("game over!");
+        Time.timeScale = 0;
+    }
+
+    private void RandomBounce(Transform target) {
+        Vector2 ramdomVec = Random.insideUnitSphere;
+        target.GetComponent<Rigidbody2D>().AddForce(ramdomVec.normalized * speed);
+    }
+
     private void Bounce(Collision2D other) {
         var speed = lastVelocity.magnitude;
         var direction = Vector3.Reflect(lastVelocity.normalized, other.contacts[0].normal);
 
         rigidbody.velocity = direction * speed;
+    }
+
+    private void AddBall() {
+        GameObject ball = Instantiate(ballPrefabs[Random.Range(0, ballPrefabs.Length)]);
+        RandomBounce(ball.transform);
     }
 }
